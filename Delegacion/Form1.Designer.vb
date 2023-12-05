fe7a87e7c93ee8d803a0fc4c0dc049ebfaaa784a
iizeta <jiz.j12.a4@gmail.com>
2023-12-05 13:12:00 +0100

    RESETEO DE TODO

--- Delegacion/Form1.Designer.vb
+++ Delegacion/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()>
+    <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()>
+    <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.GestionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -37,12 +37,10 @@ Partial Class Form1
         Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-<<<<<<< HEAD
-        Me.Button2 = New System.Windows.Forms.Button()
-=======
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
->>>>>>> origin/main
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -133,11 +131,7 @@ Partial Class Form1
         '
         'Button1
         '
-<<<<<<< HEAD
-        Me.Button1.Location = New System.Drawing.Point(561, 82)
-=======
-        Me.Button1.Location = New System.Drawing.Point(179, 62)
->>>>>>> origin/main
+        Me.Button1.Location = New System.Drawing.Point(391, 157)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
@@ -147,58 +141,57 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-<<<<<<< HEAD
-        Me.Label1.Location = New System.Drawing.Point(702, 154)
-=======
-        Me.Label1.Location = New System.Drawing.Point(193, 172)
->>>>>>> origin/main
+        Me.Label1.Location = New System.Drawing.Point(377, 260)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(48, 16)
+        Me.Label1.Size = New System.Drawing.Size(55, 16)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "maricon"
         '
-        'ListBox1
+        'Label2
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 16
-<<<<<<< HEAD
-        Me.ListBox1.Location = New System.Drawing.Point(561, 144)
-=======
-        Me.ListBox1.Location = New System.Drawing.Point(26, 51)
->>>>>>> origin/main
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(104, 260)
-        Me.ListBox1.TabIndex = 3
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(373, 217)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "maricon"
         '
-<<<<<<< HEAD
-        'Button2
+        'Label3
         '
-        Me.Button2.Location = New System.Drawing.Point(705, 277)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-=======
-        'TextBox1
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(516, 185)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "maricon"
         '
-        Me.TextBox1.Location = New System.Drawing.Point(154, 258)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox1.TabIndex = 4
->>>>>>> origin/main
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(461, 236)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "maricon"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(475, 286)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(55, 16)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "maricon"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-<<<<<<< HEAD
-        Me.Controls.Add(Me.Button2)
-=======
-        Me.Controls.Add(Me.TextBox1)
->>>>>>> origin/main
-        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.MenuStrip1)
@@ -228,10 +221,8 @@ Partial Class Form1
     Friend WithEvents ImprimirToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
-    Friend WithEvents ListBox1 As ListBox
-<<<<<<< HEAD
-    Friend WithEvents Button2 As Button
-=======
-    Friend WithEvents TextBox1 As TextBox
->>>>>>> origin/main
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 End Class
--- Delegacion/Form1.vb
+++ Delegacion/Form1.vb
@@ -1,16 +1,5 @@
-﻿Imports System.Drawing.Drawing2D
-
-Public Class Form1
+﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
-        ' Configura el degradado
-        Dim rect As New Rectangle(0, 0, Me.Width, Me.Height)
-        Dim brush As New LinearGradientBrush(rect, Color.Blue, Color.White, LinearGradientMode.Vertical)
-
-        ' Dibuja el degradado en el fondo del formulario
-        e.Graphics.FillRectangle(brush, rect)
     End Sub
 End Class

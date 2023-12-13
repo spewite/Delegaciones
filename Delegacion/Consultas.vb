555d3217d717f07ed72a89d0bf852c3403d492c7
Ander <andercete@gmail.com>
2023-12-13 09:59:06 +0100

    EL BUENO

--- Delegacion/Consultas.vb
+++ Delegacion/Consultas.vb
@@ -1,14 +1,7 @@
 ﻿Public Class Consultas
-<<<<<<< HEAD
-
-=======
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim dataTableGeneral = Dllgaciones.BaseDeDatos.ConsultaBBDD("SELECT * FROM ESTADO_PEDIDOS")
-        dataGridPedidos.DataSource = dataTableGeneral
-    End Sub
->>>>>>> d1c5344 (Primera implementacion de la dll y consultar datagrid)
+
 End Class

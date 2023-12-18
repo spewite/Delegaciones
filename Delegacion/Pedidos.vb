242837595c3d626116cca186c8a1726466f09f9c
Ander <andercete@gmail.com>
2023-12-18 11:05:26 +0100

    PUÑEEEEEETAAAA

--- Delegacion/Pedidos.vb
+++ Delegacion/Pedidos.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Pedidos
-
+    Private Sub Pedidos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+        Me.ControlBox = False
+    End Sub
 End Class
